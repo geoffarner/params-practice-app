41f912cd6a64a3d12a1e294a0ef5f0f87d6b90a7
Geoffrey Arner <geoffarner5@gmail.com>
2022-07-06 08:34:47 -0500

    Planting seeds

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -2,4 +2,9 @@ class ParamsController < ApplicationController
   def query
     render json: ("hello")
   end
+
+  def index
+    params = Param.all
+    render json: params.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/params" => "params#query"
+  get "/index" => "params#index"
 end
